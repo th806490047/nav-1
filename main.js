1580c43f2a5c454a42af746e6addd31fe14f0877
th806490047 <806490047@163.com>
2021-10-29 16:03:08 +0800

    my nav V1.0
    Signed-off-by: th806490047 <806490047@163.com>

---
+++ main.js
@@ -0,0 +1,82 @@
+const $siteList = $(".siteList");
+const $LiLast = $siteList.find("li.last");
+const x = localStorage.getItem("x");
+const xObject = JSON.parse(x);
+const hashMap = xObject || [
+  {
+    logo: "./images/jirengu.png",
+    logoType: "image",
+    url: "https://jirengu.com/",
+  },
+  {
+    logo: "./images/Bzhan.png",
+    logoType: "image",
+    url: "https://www.baidu.com/",
+  },
+];
+
+const simplifyUrl = (url) => {
+  return url
+    .replace("https://", "")
+    .replace("http://", "")
+    .replace("www.", "")
+    .replace(/\/.*/, ""); // 删除 / 开头的内容
+};
+
+const render = () => {
+  $siteList.find("li:not(.last)").remove();
+  hashMap.forEach((node, index) => {
+    const $li = $(`
+      <li>
+          <div class="site">
+            <div class="logo">${node.logo[0]}</div>
+            <div class="link">${simplifyUrl(node.url)}</div>
+            <div class="close">
+            <svg t="1635410144865" class="icon" viewBox="0 0 1045 1024" version="1.1" xmlns="http://www.w3.org/2000/svg" p-id="813" width="200" height="200"><path d="M282.517333 213.376l-45.354666 45.162667L489.472 512 237.162667 765.461333l45.354666 45.162667L534.613333 557.354667l252.096 253.269333 45.354667-45.162667-252.288-253.44 252.288-253.482666-45.354667-45.162667L534.613333 466.624l-252.096-253.226667z" p-id="814"></path></svg>
+            
+            </div>
+          </div>
+    </li>
+    `).insertBefore($LiLast);
+    $li.on("click", () => {
+      window.open(node.url);
+    });
+    $li.on("click", ".close", (e) => {
+      e.stopPropagation(); // 阻止冒泡
+      hashMap.splice(index, 1);
+      render();
+    });
+  });
+};
+
+render();
+
+$(".addButton").on("click", () => {
+  console.log("点了我？");
+  let url = window.prompt("请问你需要女人？");
+  if (url.indexOf("http") !== 0) {
+    url = "http://" + url;
+  }
+  console.log(url);
+  hashMap.push({
+    logo: simplifyUrl(url)[0].toUpperCase(),
+    url: url,
+  });
+  render();
+});
+
+window.onbeforeunload = () => {
+  console.log("页面就要关闭了");
+  const string = JSON.stringify(hashMap);
+  localStorage.setItem("x", string);
+};
+
+$(document).on('keypress',(e)=>{
+  const {key} = e
+  for(let i = 0 ; i < hashMap.length ; i++){
+    if( hashMap[i].logo.toLowerCase() === key ){
+      console.log(hashMap[i])
+      window.open(hashMap[i].url)
+    }
+  }
+})
